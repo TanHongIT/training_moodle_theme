520ad8ac422cd01b7258b2fefb40ad3bc2b96b87
tanhongit <phuongtan12357nguyen@gmail.com>
2021-03-19 15:16:24 +0700

    Add some new settings

--- lib.php
+++ lib.php
@@ -18,28 +18,34 @@ function theme_trainingmoodletheme_get_pre_scss($theme)
 }
 
 
-function theme_trainingmoodletheme_get_main_scss_content($theme) {                                                                                
-    global $CFG;                                                                                                                    
- 
-    $scss = '';                                                                                                                     
-    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;                                                 
-    $fs = get_file_storage();                                                                                                       
- 
-    $context = context_system::instance();                                                                                          
-    if ($filename == 'default.scss') {                                                                                              
+function theme_trainingmoodletheme_get_main_scss_content($theme)
+{
+    global $CFG;
+
+    $scss = '';
+    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
+    $fs = get_file_storage();
+
+    $context = context_system::instance();
+    if ($filename == 'default.scss') {
         // We still load the default preset files directly from the boost theme. No sense in duplicating them.                      
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');                                        
-    } else if ($filename == 'plain.scss') {                                                                                         
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    } else if ($filename == 'plain.scss') {
         // We still load the default preset files directly from the boost theme. No sense in duplicating them.                      
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');                                          
- 
-    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_trainingmoodletheme', 'preset', 0, '/', $filename))) {              
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_trainingmoodletheme', 'preset', 0, '/', $filename))) {
         // This preset file was fetched from the file area for theme_trainingmoodletheme and not theme_boost (see the line above).                
-        $scss .= $presetfile->get_content();                                                                                        
-    } else {                                                                                                                        
+        $scss .= $presetfile->get_content();
+    } else {
         // Safety fallback - maybe new installs etc.                                                                                
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');                                        
-    }                                                                                                                                       
- 
-    return $scss;                                                                                                                   
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    }
+    
+    // Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.                                        
+    $pre = file_get_contents($CFG->dirroot . '/theme/trainingmoodletheme/scss/pre.scss');
+    // Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.                                    
+    $post = file_get_contents($CFG->dirroot . '/theme/trainingmoodletheme/scss/post.scss');
+
+    // Combine them together.                                                                                                       
+    return $pre . "\n" . $scss . "\n" . $post;
 }
